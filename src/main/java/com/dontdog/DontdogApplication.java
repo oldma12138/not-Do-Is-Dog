13bac4fad3328eb3d67ba639620a9dd6e6240af8
fatYch <897141839@qq.com>
2021-01-04 21:11:47 +0800

    feat: 冲

---
+++ src/main/java/com/dontdog/DontdogApplication.java
@@ -0,0 +1,13 @@
+package com.dontdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontdogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DontdogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dontdog/DontdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontdogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
